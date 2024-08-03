8b31739a471d6f3afc85fade05ddedd4c24b9e24
VAIBHAV PATHAK <129991855+Vaibhav007-code@users.noreply.github.com>
2024-08-03 13:13:03 +0530

    Time: 7 ms (84.2%), Space: 17.5 MB (42.71%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -3,6 +3,11 @@ public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
         sort(target.begin(),target.end());
         sort (arr.begin(),arr.end());
-        return target == arr;
+        for(int i = 0;i<target.size();i++ ){
+            if (target[i] != arr[i]){
+                return false;
+            }
+        }
+        return true;
     }
 };
