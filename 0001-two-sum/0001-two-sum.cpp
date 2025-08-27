db99bcdd848798227cf2dff19b6fc4906658b188
VAIBHAV PATHAK <129991855+Vaibhav007-code@users.noreply.github.com>
2025-08-27 22:18:32 +0530

    Time: 2 ms (75.81%), Space: 14.8 MB (36.15%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        for (int i = 0 ; i <n ;i++){
-            for (int j =i+1;j<n;j++){
-            if (nums[i]+nums[j]==target){
-                return {i,j};
-                
+        //declare an umordered map
+        unordered_map <int ,int>mp;
+
+        //run a loop till the end of the array , nums.end()
+        for (int i = 0  ; i < nums.size();i++){
+
+            //create a variable to store the missing value which when added make total= target
+            //eg = target = 7 , nums[i] = 2 , miss = ?
+            // to find miss  we do , miss = target-nums[i] which is  = 5
+            int com = target - nums[i];
+
+            if (mp.count(com)){
+                return {mp[com], i };
+
             }
-            
-            }
-            
+            mp[nums[i]] = i ;
         }
+
         return {};
         
     }
