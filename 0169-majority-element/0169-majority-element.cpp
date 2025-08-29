e6dd03e2f4b0ecf9dd7a81f46d3643d08115925e
VAIBHAV PATHAK <129991855+Vaibhav007-code@users.noreply.github.com>
2025-08-29 22:34:46 +0530

    Time: 0 ms (100%), Space: 28.1 MB (62.83%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,13 +1,32 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        std::unordered_map<int,int>count;
-        for(int num:nums){
-            count[num]++;
-            if(count[num]>nums.size()/2){
-                return num;
+        int freq  =  0 , ans = 0 ;
+        
+        for (int i = 0 ; i < nums.size(); i++){
+            if (freq == 0){
+                ans = nums[i];
+            } 
+            if (ans  == nums[i]){
+                freq++;
+            }
+            else{
+                freq--;
             }
         }
-        return -1;
+        int count  = 0 ;
+        for ( int val:nums){
+              if (val == ans ){
+                count ++;
+              }
+        }
+
+        if(count > nums.size()/2){
+            return ans;
+        }
+        else{
+            return -1;
+        }
+        
     }
 };
