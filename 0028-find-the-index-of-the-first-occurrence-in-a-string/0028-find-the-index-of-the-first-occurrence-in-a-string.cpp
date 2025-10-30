06f608aec3216c592f85445224dff49797b29300
VAIBHAV PATHAK <129991855+Vaibhav007-code@users.noreply.github.com>
2025-10-30 09:38:28 +0530

    Time: 0 ms (100%), Space: 8.7 MB (30.04%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,17 +1,9 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        int helen = haystack.length();;
-        int nlen = needle.length();
-        if (nlen == 0) return 0;
-        for (int i = 0; i<=helen-nlen;i++){
-            int j;
-            for ( j = 0;j<nlen;j++){
-                if(haystack[i+j]!=needle[j]){
-                    break;
-                }
-            }
-            if (j == nlen){
+    int strStr(std::string haystack, std::string needle) {
+          if (needle.size() > haystack.size()) return -1;
+        for (int i = 0; i <= haystack.length() - needle.length(); ++i) {
+            if (haystack.substr(i, needle.length()) == needle) {
                 return i;
             }
         }
